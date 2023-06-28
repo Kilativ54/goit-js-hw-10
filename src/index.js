0b30edb5d4320ac39f0a9beb321c235ada4889dc
Kilativ54 <vitalikechkalov@gmail.com>
2023-06-28 22:16:35 +0200

    try to fix chose cat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-import axios from "axios";
+import axios from 'axios';
+import refs from './js/ref';
+import { fetchCatByBreed } from './js/cat-api';
+import { fetchBreeds } from './js/cat-api';
+import SlimSelect from 'slim-select';
+import '/node_modules/slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+import '/node_modules/slim-select/dist/slimselect.css';
+const imgCat = document.querySelector('.js-cat-img');
+const info = document.querySelector('.js-cat-info')
 
-axios.defaults.headers.common["x-api-key"] = "live_p5niNW4OOHNLdJ3o6FW7N5biXaHczYp83ylEmkqFRAsQJPeqoS7SJbu2tpmQTRFt";
+fetchBreeds()
+  .then(data => {
+    const option = data.map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>`;
+    });
+    refs.selectEl.insertAdjacentHTML('beforeend', option);
+    new SlimSelect({
+      select: '#single',
+    });
+    refs.loaderEl.hidden = true;
+  })
+  .catch(() => {
+    refs.loaderEl.hidden = true;
+  });
+
+refs.selectEl.addEventListener('change', chooseCat);
+
+function chooseCat(event) {
+  event.preventDefault();
+  let idCat = event.target.value;
+  console.log(idCat);
+  fetchCatByBreed(idCat)
+    .then(data => {
+const imgElement = document.createElement('img');
+           refs.catInfo.innerHTML =  `<img src='${data[0].url}' width='600' alt="${data[0].breeds[0].name}">
+      <div class="cat-box">
+          <h2>${data[0].breeds[0].name}</h2>
+          <p>${data[0].breeds[0].description}</p>
+      <h2>Temperament</h2>
+      <p>${data[0].breeds[0].temperament}</p>
+      </div>`;
+    })
+    .catch(error => {
+      console.log(error);
+      errorEl.classList.add('error-none');
+    });
+}
---
+++ src/js/ref.js
@@ -0,0 +1,7 @@
+const refs = {
+     selectEl: document.querySelector('.breed-select'),
+     loaderEl: document.querySelector('.loader'),
+     errorEl: document.querySelector('.error'),
+     catInfo: document.querySelector('cat-info')
+}
+export default refs;
